e6e1dd6f25274b4493c49f2c7630978c871a4749
Eddie Chou <eddieechou@gmail.com>
2017-05-15 14:03:53 -0700

    (feat) Set up directory structure and example test

---
+++ solutions/className.js
@@ -0,0 +1,13 @@
+function addClass(el, newClass) {
+  // If it doesn't exist
+  if(el.className.indexOf(newClass) !== -1) {
+    return;
+  }
+
+  if(el.className !== '') {
+    //ensure class names are separated by a space
+    newClass = ' ' + newClass;
+  }
+
+  el.className += newClass;
+}
