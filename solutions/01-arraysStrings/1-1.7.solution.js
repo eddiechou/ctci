364b70f7731917a665458a0dc5de096f25d70394
Eddie Chou <eddieechou@gmail.com>
2017-05-16 14:56:48 -0700

    (feat) Implement rotateMatrix to rotate a matrix 90 degree in place

--- solutions/01-arraysStrings/1-1.7.solution.js
+++ solutions/01-arraysStrings/1-1.7.solution.js
@@ -3,5 +3,25 @@
 // Rotates matrix in place
 
 function rotateMatrix(matrix) {
+  var n = matrix[0].length;
+  // for each layer
+  for (let layer = 0; layer < n / 2; layer++) {
 
+    var first = layer;
+    var last = n - 1 - layer;
+
+    for (let i = first; i < last; i++) {
+      var offset = i - first;
+      // save top
+      var top = matrix[first][i];
+      // left -> top
+      matrix[first][i] = matrix[last - offset][first];
+      // bot -> left
+      matrix[last - offset][first] = matrix[last][last - offset];
+      // right -> bot
+      matrix[last][last - offset] = matrix[i][last]
+      // set right as top
+      matrix[i][last] = top;
+    }
+  } 
 }
